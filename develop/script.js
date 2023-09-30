c94f3630952fa265f2f17a34bbd9367f94f37aaa
Ryan Mattson <ryan.mattson32@gmail.com>
2023-09-30 14:23:47 -0600

    Finished script.js

--- develop/script.js
+++ develop/script.js
@@ -8,6 +8,7 @@ $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     const currentHour = dayjs().format('H');
  
+    function hourlyColor() {
       $('.time-block').each(function() {
         const blockHour = parseInt(this.id);
         $(this).toggleClass('past', blockHour < currentHour);
@@ -19,10 +20,47 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    // function textEntry() {
+      function textEntry() {
+        $('.saveBtn').on('click', function() {
+          const key = $(this).parent().attr('id');
+          const value = $(this).siblings('.description').val();
+          localStorage.setItem(key, value);
+        });
+      }
+    }
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+    // 
+    function refreshColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        if (blockHour == currentHour) {
+          $(this).removeClass('past future').addClass('present');
+        } else if (blockHour < currentHour) {
+          $(this).removeClass('future present').addClass('past');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
     // TODO: Add code to display the current date in the header of the page.
-  });
+    $('.time-block').each(function() {
+      const key = $(this).attr('id');
+      const value = localStorage.getItem(key);
+      $(this).children('.description').val(value);
+    });
+     
+    const dateElement = $('#date');
+    const timeElement = $('#time');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    const currentTime = dayjs().format('hh:mm:ss A');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  },
+  hourlyColor(),
+  textEntry(),               
+  refreshColor(),
+
+  setInterval(updateTime, 1000))
